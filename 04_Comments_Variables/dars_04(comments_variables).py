5bffea21f055759f3ac9c9b437ad5c1f38271f34
Urinov Azizbek <63332872+zero-suger@users.noreply.github.com>
2024-01-25 20:52:27 +0900

    Add files via upload

---
+++ 04_Comments_Variables/dars_04(comments_variables).py
@@ -0,0 +1,49 @@
+# PYTHON 4-DARS : Comment yozish va Variables nima ? 
+
+# Comments : Bir qator va ko'p qatorlik
+
+# Python dars 4 
+
+"""
+Assalomu Aleykum
+Python ga Xush kelibsz
+Azizbek
+"""
+
+
+
+# variables (vaqtincha value saqlash) : Camel case, Snake case va Pascal case (yozish usullari)
+
+# VARIABLES turlari
+
+_userName = "Azizbek" # Camel case
+print(_userName)
+
+user_name = "Aliy" # Snake case
+print(user_name)
+
+UserName = "Murodjon" # Pascal case
+print(user_name)
+
+
+
+
+
+""""
+    O'zgaruvchi nomi harf yoki pastki chiziq belgisi bilan boshlanishi kerak
+    O'zgaruvchi nomi raqam bilan boshlana olmaydi
+    Oʻzgaruvchi nomi faqat alfa-raqamli belgilar va pastki chiziqdan iborat boʻlishi mumkin (A-z, 0-9 va _ )
+    O'zgaruvchilar nomlari katta-kichik harflarga sezgir (yosh, Age va AGE uch xil o'zgaruvchidir)
+    O'zgaruvchi nomi Python kalit so'zlaridan birortasi bo'lishi mumkin emas.
+
+"""
+
+Age = 4
+age = 5
+AGE = 6
+
+# print(Age)
+
+
+print = 4 
+print(print)
